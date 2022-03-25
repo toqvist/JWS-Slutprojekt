3a5f12a722744accd925557398559a5ca389109e
toqvist <tobias-ahlqvist@outlook.com>
2022-03-25 23:08:24 +0100

    Mapped greeting page to /

--- slutprojekt/src/main/java/com/example/slutprojekt/GreeterController.java
+++ slutprojekt/src/main/java/com/example/slutprojekt/GreeterController.java
@@ -1,24 +1,15 @@
 package com.example.slutprojekt;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GreeterController {
  
     @RequestMapping("/")
     public String greet() {
-        String htmlString = "";
-        htmlString += "<!DOCTYPE html>";
-        htmlString += "<html lang=\"en\">";
-        htmlString += "<head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Page not found</title></head>";
-        htmlString += "<body><h1>Welcome to the API!</h1><h2>Valid endpoints:</h2><ul>";
-        htmlString += "<li>/calc - accepts parameters , value1 and value2</li>";
-        htmlString += "<li>/RSP - <li>";
-        htmlString += "<li>/Rock-paper-scissors - accepts parameters OPERATION, value1 and value2</li>";
-        htmlString += "</ul></body></html>";
-        return htmlString;
+        return "index.html";
     }
-    
+   
 
 }
