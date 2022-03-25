d37c62629b5880fdc925707e7946fee8aced5fc2
toqvist <tobias-ahlqvist@outlook.com>
2022-03-25 21:19:02 +0100

    Copied over calculator code from previous exercises

---
+++ slutprojekt/src/main/java/com/example/slutprojekt/CalculatorController.java
@@ -0,0 +1,35 @@
+package com.example.slutprojekt;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+
+@RestController
+public class CalculatorController implements ErrorController{
+    
+	private CalculatorService cs = new CalculatorService();
+
+	//http://localhost:8080/math/?operation=addition&value1=1&value2=2
+	@CrossOrigin
+	@RequestMapping("/math/")
+	public String math(String operation, float value1, float value2) {
+		return cs.math(operation, value1, value2);
+	}    
+
+	@RequestMapping(value = "/error", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public String errorPage() {
+		String htmlString = "";
+		htmlString += "<!DOCTYPE html>";
+		htmlString += "<html lang=\"en\">";
+		htmlString += "<head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Page not found</title></head>";
+		htmlString += "<body><h1>Page does not exist</h1><h2>Valid endpoints:</h2><ul>";
+		htmlString += "<li>/math - accepts parameters OPERATION, value1 and value2</li></ul></body>";
+		htmlString += "</html>";
+		return htmlString;
+	}
+
+}
---
+++ slutprojekt/src/main/java/com/example/slutprojekt/SlutprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.slutprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlutprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlutprojektApplication.class, args);
+	}
+
+}
